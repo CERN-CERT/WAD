7e77ca6f9c224daf2b1950ba68b79824c96fe126
Piotr Lizończyk <piotr.lizonczyk@gmail.com>
2015-08-04 15:43:32 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, ImportError):
 
 setup(
     name='wad',
-    version='0.1.0',
+    version='0.1.1',
     description='A tool for detecting technologies used by web applications.',
     long_description=long_description,
     url='https://github.com/CERN-CERT/WAD',
